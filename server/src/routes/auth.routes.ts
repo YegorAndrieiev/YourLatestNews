7b70c01e44a2d5fe416270854e21d507a5df2900
YegorAndrieiev <yegorandrieiev@gmail.com>
2026-08-12 22:05:13 +0300

    fix(auth): configure cross-site cookies for production

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -50,14 +50,14 @@ router.get(
       await redisClient.setEx(redisKey, 604800, JSON.stringify(sessionData));
       res.cookie('accessToken', accessToken, {
         httpOnly: true,
-        secure: false,
-        sameSite: 'lax',
+        secure: true, // use for local development: false
+        sameSite: 'none',
         maxAge: 15 * 60 * 1000,
       });
       res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
-        secure: false,
-        sameSite: 'lax',
+        secure: true, // use for local development: false
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
       return res.redirect(url);
